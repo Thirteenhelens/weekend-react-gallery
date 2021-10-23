99fe9b61ba8391451b17b2a3dfbc089fd34fc1bf
Jack Barrett <jbarrett1347@gmail.com>
2021-10-23 11:51:41 -0500

    Added terenary render for the love counter

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,16 +1,34 @@
-// `GalleryItem.jsx` and pass it the individual gallery item via `props`. 
+// `GalleryItem.jsx` and pass it the individual gallery item via `props`.
 //     - Update the `GalleryList` to use this component to display an image.
 //     - Swap the image with the description on click. Use [conditional rendering](https://reactjs.org/docs/conditional-rendering.html).
 //     - Display the number likes for each item and include a like button.
 //     - When the like button is clicked, use `Axios` to update (`PUT`) the like count `/gallery/like/:id`.
 //     - Update the gallery each time a like button is clicked.
-
+import { useState } from "react";
 
 function GalleryItem({ picture, getGalleryList }) {
+  const [loveCount, setLoveCount] = useState(0);
+
+  const loveImage = () => {
+    console.log(`I love ->`, picture.id);
+    setLoveCount(loveCount + 1);
+  };
+
   return (
     <div>
+        
       <img src={picture.path}></img>
+
       <p>{picture.description}</p>
+      
+      <button onClick={loveImage}>Lovely!</button>
+      
+      {loveCount === 0 ? (
+        <p>No love for this</p>
+      ) : (
+        <p>{loveCount} Love</p>
+      )}
+
     </div>
   );
 }
